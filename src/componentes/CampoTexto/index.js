5a7c09d3591c9d4e8f729c9e28bf756ad918795b
Wiliam Roza <devfrontwill@gmail.com>
2023-03-29 11:37:33 -0300

    Criado e estilizado parte do componente formulario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,11 +1,13 @@
 import './Campotexto.css'
 
-const CampoTexto = () => {
+const CampoTexto = (props) => {
     return (
 
         <div className="campo-texto">
-            <label>Nome</label>
-            <input placeholder='Digite o seu nome' />
+            <label>
+                {props.label}
+            </label>
+            <input placeholder= {props.placeholder} />
         </div>
     )
 }
