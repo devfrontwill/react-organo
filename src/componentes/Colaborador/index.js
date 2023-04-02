7ddade4da217106cd33ed612a99bea8248b775cf
Wiliam Roza <devfrontwill@gmail.com>
2023-04-02 16:09:05 -0300

    Criado o componente time e feito algumas configurações

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,19 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+
+    return (
+        <>
+            <div>
+                <img src='https://github.com/devfrontwill.png' alt='profile photo' />
+            </div>
+
+            <div>
+                <h4> Wiliam Roza </h4>
+                <h5> Font-End Developer </h5>
+            </div>
+        </>
+    )
+}
+
+export default Colaborador
