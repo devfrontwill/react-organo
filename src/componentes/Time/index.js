65cfa845ee812e22cfb987e983018e46d45da975
Wiliam Roza <devfrontwill@gmail.com>
2023-04-02 13:15:46 -0300

    Criado a estilização do component Time

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,8 +1,12 @@
 import './Time.css'
 
 
-const Time = () => {
-
+const Time = (props) => {
+    return (
+        <section className='time'>
+            <h3>{props.nome}</h3>
+        </section>
+    )
 
 }
 
