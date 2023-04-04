6a1ca932777982e9338f15381d1ebcfa6251625b
Wiliam Roza <devfrontwill@gmail.com>
2023-04-03 23:54:18 -0300

    Criado o componente Colaborador e feito a logica e codigo para adicionar novos membros dinamicamente

--- src/App.js
+++ src/App.js
@@ -7,45 +7,35 @@ function App() {
 
   const times = [
     {
-
-    },
-
-    {
       nome: 'Programação',
       corPrimaria: '#57C278',
       corSecundaria: '#D9F7E9'
     },
-
     {
       nome: 'Front-End',
       corPrimaria: '#82CFFA',
       corSecundaria: '#E8F8FF'
     },
-
     {
       nome: 'Data Science',
       corPrimaria: '#A6D157',
       corSecundaria: '#F0F8E2'
     },
-
     {
       nome: 'Devops',
       corPrimaria: '#E06B69',
       corSecundaria: '#FDE7E8'
     },
-
     {
       nome: 'UX e Design',
       corPrimaria: '#DB6EBF',
       corSecundaria: '#FAE9F5'
     },
-
     {
       nome: 'Mobile',
       corPrimaria: '#FFBA05',
       corSecundaria: '#FFF5D9'
     },
-
     {
       nome: 'Inovação e Gestão',
       corPrimaria: '#FF8A29',
@@ -56,7 +46,7 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    
+    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
 
@@ -64,9 +54,14 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />
-      )}
+
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}   
 
     </div>
   );
--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,19 +1,15 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
-
-    return (
-        <>
-            <div>
-                <img src='https://github.com/devfrontwill.png' alt='profile photo' />
-            </div>
-
-            <div>
-                <h4> Wiliam Roza </h4>
-                <h5> Font-End Developer </h5>
-            </div>
-        </>
-    )
+const Colaborador = ({ nome, imagem, cargo }) => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src={imagem} alt={nome}/>
+        </div>
+        <div className='rodape'>
+            <h4>{nome}</h4>
+            <h5>{cargo}</h5>
+        </div>
+    </div>)
 }
 
 export default Colaborador
